61acf7357a90d918a2e25665f4d5b6c45aeee4af
dax <d.dax@email.com>
2022-07-22 10:55:12 +0200

    Update annotation

--- lib/models/user_activity.rb
+++ lib/models/user_activity.rb
@@ -1,16 +1,21 @@
 # primary_key :id
 #
-# String  :address,     null: false
-# String  :coordinates, null: false
-# String  :name,        null: false
-# Time    :start_time,  null: false
-# Time    :end_time,    null: false
-# Integer :duration,    null: false
+# String  :name,                  null: false
+# String  :address,               null: false
+# String  :starting_coordinates,  null: false
+# String  :starting_position,     null: false
+# String  :ending_coordinates,    null: false
+# String  :ending_position,       null: false
+# Time    :start_time,            null: false
+# Time    :end_time,              null: false
+# Integer :duration,              null: false
 #
-# Time    :created_at,  null: false
+# Time    :created_at,            null: false
 #
+# add_index :user_activities, [:name]
 # add_index :user_activities, [:address]
-# add_index :user_activities, [:coordinates]
+# add_index :user_activities, [:starting_coordinates]
+# add_index :user_activities, [:ending_coordinates]
 
 module Models
   class UserActivity < Sequel::Model
