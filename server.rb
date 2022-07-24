3ade16cc9b12ed7fdfb771b903b93e1d49a74fd3
dax <d.dax@email.com>
2022-07-24 20:56:34 +0200

    Add a "top" sort

--- server.rb
+++ server.rb
@@ -9,30 +9,44 @@ class Server < Sinatra::Application
     Models::DailyStats.recent.map(&:serialize).to_json
   end
 
-  get '/api/user_stats/:attribute' do
+  get '/api/user_stats/:attribute/:sort' do
     attribute = params[:attribute].to_sym
 
     unless %i[time_spent parcels_visited].include?(attribute)
       status 400
-      return { msg: "#{attribute.to_s} is not valid." }.to_json
+      return { msg: "'#{attribute.to_s}' is not valid." }.to_json
     end
 
-    Models::DailyUserStats.
-      recent.
-      exclude(attribute => nil).
-      reverse_order(attribute).
-      all.
-      group_by { |stats| stats.date.to_s }.
-      sort_by(&:first).
-      to_h.
-      transform_values! { |v| v.map(&:serialize) }.
-      to_json
+    case params[:sort]
+    when 'daily'
+      Models::DailyUserStats.
+        recent.
+        exclude(attribute => nil).
+        reverse_order(attribute).
+        all.
+        group_by { |stats| stats.date.to_s }.
+        sort_by(&:first).
+        to_h.
+        transform_values! { |v| v.map(&:serialize) }.
+        to_json
+    when 'top'
+      Models::DailyUserStats.
+        recent.
+        exclude(attribute => nil).
+        all.
+        group_by(&:address).
+        map { |address, data| { address: address, time_spent: data.sum(&:time_spent) } }.
+        to_json
+    else
+      status 400
+      return { msg: "Sort parameter '#{params[:sort]}' is not valid." }.to_json
+    end
   end
 
   get '/api/parcel_stats/:attribute' do
     unless %w[time_spent visitors logins logouts].include?(params[:attribute])
       status 400
-      return { msg: "#{params[:attribute]} is not valid." }.to_json
+      return { msg: "'#{params[:attribute]}' is not valid." }.to_json
     end
 
     attribute = map_parcel_attribute(params[:attribute])
