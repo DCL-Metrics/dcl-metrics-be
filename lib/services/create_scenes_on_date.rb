65afa9da8e488eb349f2f40f647ee7926e017cb4
dax <d.dax@email.com>
2022-07-30 14:32:56 +0200

    Extract fetch_scene_data service

--- lib/services/create_scenes_on_date.rb
+++ lib/services/create_scenes_on_date.rb
@@ -1,14 +1,11 @@
 module Services
   class CreateScenesOnDate
-    URL = 'https://peer.decentraland.org/content/entities/scene?pointer='
-
     def self.call(date:)
       new(date).call
     end
 
     def initialize(date)
       @date = date
-      @scenes = []
     end
 
     def call
@@ -16,24 +13,7 @@ module Services
         "select distinct coordinates from data_points where date = '#{date}'"
       ].flat_map(&:values)
 
-      coordinates.each do |c|
-        # check if coordinates are part of an existing scene
-        next if scenes.any? { |s| s[:parcels].include?(c) }
-
-        # get data from url
-        scene_data = JSON.parse(`curl -s #{URL}#{c}`)[0]
-
-        # skip if there is no scene at these cooridinates
-        next if scene_data.nil? || scene_data.empty?
-
-        # add scene to scenes
-        scenes << {
-          id: scene_data['id'],
-          name: scene_data['metadata']['display']['title'],
-          owner: scene_data['metadata']['owner'],
-          parcels: scene_data['pointers']
-        }
-      end
+      scenes = Services::FetchSceneData.call(coordinates: coordinates)
 
       # create scene list
       Models::SceneList.create(
@@ -44,6 +24,5 @@ module Services
 
     private
     attr_reader :date
-    attr_accessor :scenes
   end
 end
---
+++ lib/services/fetch_scene_data.rb
@@ -0,0 +1,35 @@
+
+module Services
+  class CreateScenesOnDate
+    URL = 'https://peer.decentraland.org/content/entities/scene'
+
+    def self.call(coordinates:)
+      new(coordinates).call
+    end
+
+    def initialize(coordinates)
+      @coordinates = coordinates
+    end
+
+    def call
+      scenes = []
+      pointers = coordinates.map { |x| "pointer=#{x}" }.join('&')
+      request = `curl -s -G #{URL} -d "query=#{pointers}"`
+      scene_data = JSON.parse(request).uniq { |s| s['id'] }
+
+      scene_data.each do |scene|
+        scenes.push({
+          id: scene_data['id'],
+          name: scene_data['metadata']['display']['title'],
+          owner: scene_data['metadata']['owner'],
+          parcels: scene_data['pointers']
+        })
+      end
+
+      scenes
+    end
+
+    private
+    attr_reader :coordinates
+  end
+end
