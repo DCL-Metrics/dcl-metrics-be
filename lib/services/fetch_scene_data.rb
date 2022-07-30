e47a0307bc381aead5820b8b4281f16bb5ed7057
dax <d.dax@email.com>
2022-07-30 15:21:35 +0200

    Fix fetch_scene_data

--- lib/services/fetch_scene_data.rb
+++ lib/services/fetch_scene_data.rb
@@ -12,17 +12,36 @@ module Services
 
     def call
       scenes = []
-      pointers = coordinates.map { |x| "pointer=#{x}" }.join('&')
-      request = `curl -s -G #{URL} -d "query=#{pointers}"`
-      scene_data = JSON.parse(request).uniq { |s| s['id'] }
 
-      scene_data.each do |scene|
-        scenes.push({
-          id: scene_data['id'],
-          name: scene_data['metadata']['display']['title'],
-          owner: scene_data['metadata']['owner'],
-          parcels: scene_data['pointers']
-        })
+      coordinates.sort.each_slice(20) do |batch|
+        # check if coordinates are part of an existing scene
+        c = batch - scenes.flat_map { |s| s[:parcels] }
+        next if c.empty?
+
+        # get data from url
+        # if there is only one element in the array the request needs
+        # to be in a different format for whatever reason :shrug:
+        if c.count == 1
+          request = `curl -s "#{URL}?pointer=#{c.join(',')}"`
+        else
+          pointers = c.map { |x| "pointer=#{x}" }.join('&')
+          request = `curl -s -G #{URL} -d "query=#{pointers}"`
+        end
+
+        scene_data = JSON.parse(request).compact
+
+        scene_data.each do |scene|
+          next if scene.nil?
+          next if scene.empty?
+          next if scenes.detect { |s| s['id'] == scene['id'] }
+
+          scenes.push({
+            id: scene['id'],
+            name: scene['metadata']['display']['title'],
+            owner: scene['metadata']['owner'],
+            parcels: scene['pointers']
+          })
+        end
       end
 
       scenes
