a0aca2ec4ba44165a513971917da6d685c949cf7
dax <d.dax@email.com>
2022-08-19 12:37:48 +0200

    Send all sinatra errors to sentry

--- server.rb
+++ server.rb
@@ -3,8 +3,8 @@ require 'sinatra'
 class Server < Sinatra::Application
   ALLOWED_ACCESS_IP = %w[99.80.183.117 99.81.135.32]
 
+  # Ensure all requests come from a fixed IP
   before do
-    p request.env
     requesting_ip = request.env["HTTP_X_FORWARDED_FOR"] || request.env['REMOTE_ADDR']
 
     unless ALLOWED_ACCESS_IP.include?(requesting_ip)
@@ -15,6 +15,11 @@ class Server < Sinatra::Application
 
       halt 401, { msg: "I'm afraid I can't let you do that, #{requesting_ip}" }.to_json
     end
+  end
+
+  # Send all exceptions to sentry
+  error Exception do
+    Sentry.capture_exception(env['sinatra.error'])
   end
 
   get '/' do
