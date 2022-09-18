801a90b914dac28353e699160df2e5ba039c1e44
dax <d.dax@email.com>
2022-09-18 15:59:49 +0200

    Refactor and fix bugs

--- lib/jobs/process_daily_scene_stats.rb
+++ lib/jobs/process_daily_scene_stats.rb
@@ -22,28 +22,46 @@ module Jobs
       unique_visitors = visits.distinct(:address).count
       unique_addresses = scene_traffic.flat_map(&:addresses).uniq.count
 
-      # "x% of users that visited dcl today visited this scene"
-      share_of_global_visitors = (unique_visitors / total_global_users.to_f) * 100
+      if visits.any?
+        # "x% of users that visited dcl today visited this scene"
+        share_of_global_visitors = (unique_visitors / total_global_users.to_f) * 100
 
-      # avg_time_spent
-      avg_time_spent = if visits.any?
-         total_visit_duration_seconds = visits.sum(:duration)
-         (total_visit_duration_seconds / 60) / total_visitors.to_f
-       else
-         0
-       end
+        # avg_time_spent
+        total_visit_duration_seconds = visits.sum(:duration).to_i
+        avg_time_spent = if total_visit_duration_seconds.zero?
+          0
+        else
+          (total_visit_duration_seconds / 60.to_f) / total_visitors
+        end
 
-      if afk.any?
         # avg_time_spent_afk
-        total_afk_duration_seconds = afk.sum(:duration)
-        avg_time_spent_afk = (total_afk_duration_seconds / 60) / total_visitors.to_f
+        total_afk_duration_seconds = afk.sum(:duration).to_i
+        avg_time_spent_afk = if total_afk_duration_seconds.zero?
+          0
+        else
+          (total_afk_duration_seconds / 60.to_f) / total_visitors
+        end
 
         # % of afk users
         total_afk_users = afk.distinct(:address).count
         percent_of_users_afk = (total_afk_users / unique_visitors.to_f) * 100
+
+        # users with longest session
+        visits_by_address = visits.all.group_by(&:address)
+        visitors_by_duration = visits_by_address.
+          map { |address, visits| [address, visits.map(&:duration).sum.to_i / 60.to_f] }.
+          sort_by(&:last)
+
+        user_visit_histogram = visitors_by_duration.
+          group_by { |address, duration| (duration / 60.to_f).floor }.
+          map { |k, v| [k, v.size] }
       else
+        share_of_global_visitors = 0
+        avg_time_spent = 0
         avg_time_spent_afk = 0
         percent_of_users_afk = 0
+        visitors_by_duration = []
+        user_visit_histogram = []
       end
 
       # logins:
@@ -62,30 +80,18 @@ module Jobs
 
       # complete sessions (user logged in and logged out from this scene - not unique):
       complete_sessions = scene_activities.where(name: 'session')
-      avg_complete_session_duration = if complete_sessions.any?
-        total_duration = complete_sessions.sum(:duration) / 60
-        total_duration / complete_sessions.count.to_f
-      else
+      total_duration = complete_sessions.sum(:duration).to_i
+      avg_complete_session_duration = if total_duration.zero?
         0
+      else
+        (total_duration / 60.to_f) / complete_sessions.count
       end
-
-      # users with longest session
-      visits_by_address = visits.all.group_by(&:address)
-      visitors_by_duration = visits_by_address.
-        map { |address, visits| [address, visits.map(&:duration).sum / 60] }.
-        sort_by(&:last)
-
-      user_visit_histogram = visitors_by_duration.
-        group_by { |address, duration| (duration / 60.to_f).floor }.
-        map { |k, v| [k, v.size] }
 
       # parcel heatmap
       parcels_heatmap = scene_traffic.
         map { |pt| [pt.coordinates, pt.unique_addresses] }.
         to_h
 
-      # NOTE: all the "to_i"'s are important cause the db fields are integers
-      # and sequel doesn't seem to like to auto-convert
       Models::DailySceneStats.create(
         date: date,
         name: name,
@@ -94,16 +100,16 @@ module Jobs
         total_visitors: total_visitors,
         unique_visitors: unique_visitors,
         unique_addresses: unique_addresses,
-        share_of_global_visitors: share_of_global_visitors.to_i,
-        avg_time_spent: avg_time_spent.to_i,
-        avg_time_spent_afk: avg_time_spent_afk.to_i,
-        percent_of_users_afk: percent_of_users_afk.to_i,
+        share_of_global_visitors: share_of_global_visitors,
+        avg_time_spent: avg_time_spent,
+        avg_time_spent_afk: avg_time_spent_afk,
+        percent_of_users_afk: percent_of_users_afk,
         total_logins: total_logins,
         unique_logins: unique_logins,
         total_logouts: total_logouts,
         unique_logouts: unique_logouts,
         complete_sessions: complete_sessions.count,
-        avg_complete_session_duration: avg_complete_session_duration.to_i,
+        avg_complete_session_duration: avg_complete_session_duration,
         visitors_by_total_time_spent_json: visitors_by_duration.to_h.to_json,
         visitors_total_time_spent_histogram_json: user_visit_histogram.to_json,
         parcels_heatmap_json: parcels_heatmap.to_json
