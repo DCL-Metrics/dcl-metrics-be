76022a453af8dbb457fddbd8cbfbc7e890a87191
dax <d.dax@email.com>
2022-09-19 10:15:03 +0200

    Don't send pool timeout errors to telegram

--- lib/middleware/sidekiq_error_notifications.rb
+++ lib/middleware/sidekiq_error_notifications.rb
@@ -4,22 +4,25 @@ module Middleware
       begin
         yield
       rescue => e
-        Services::TelegramOperator.notify(
-          level: :error,
-          message: 'Error in sidekiq job',
-          payload: {
-            error_class: e.class,
-            error_msg: e.message,
-            job: job,
-            queue: queue,
-            worker: worker
-          }
-        )
+        send_error_to_telegram(e, job)
 
         # raising tells sidekiq to mark this job as failed
         # and move it to the retry queue
         raise
       end
+    end
+
+    def send_error_to_telegram(e, job)
+      return if IGNORABLE_ERRORS.include?(e.class)
+
+      Services::TelegramOperator.notify(
+        level: :error,
+        message: 'Error in sidekiq job',
+        payload: job.merge({
+          error_class: e.class,
+          error_msg: e.message,
+        })
+      )
     end
   end
 end
