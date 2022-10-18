7dce271331f47dace4351b95c31ade74d4a1d5a3
dax <d.dax@email.com>
2022-10-18 10:54:14 +0200

    Update output of peers_status endpoint

--- lib/serializers/peer_status.rb
+++ lib/serializers/peer_status.rb
@@ -6,23 +6,34 @@ module Serializers
 
     def initialize(api_responses)
       @api_responses = api_responses
+      @result = {}
     end
 
     def call
-      api_responses.map do |response|
+      api_responses.group_by(&:endpoint).each do |endpoint, responses|
+        result[endpoint] = build_response(responses)
+      end
+
+      result
+    end
+
+    private
+    attr_reader :api_responses, :result
+
+    def build_response(responses)
+      responses.map do |response|
         {
           date: response.date.to_s,
           url: response.url,
+          host: response.host,
           statuses: response.status_counts,
           failure_count: response.failure_count,
           failure_rate: response.failure_rate,
+          success_rate: 100 - response.failure_rate,
           success_count: response.success_count,
           total_count: response.total_count
         }
       end
     end
-
-    private
-    attr_reader :api_responses
   end
 end
