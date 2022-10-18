c7329bcb296120d65da1fbc4a785723a8e886115
dax <d.dax@email.com>
2022-10-18 14:09:19 +0200

    Force symbol syntax

--- lib/jobs/export_data_to_staging_db.rb
+++ lib/jobs/export_data_to_staging_db.rb
@@ -5,7 +5,9 @@ module Jobs
     def perform(model_name, period, table_name = nil)
       return unless ENV['RACK_ENV'] == 'production'
 
-      table_name ||= model_name.to_sym
+      # account for non-standard table names (pluralized, etc)
+      table_name ||= model_name
+
       klass_name = model_name.split('_').map(&:capitalize).join
       model = Models.const_get(klass_name)
       columns = model.columns - [:id]
@@ -17,8 +19,8 @@ module Jobs
 
       # staging db
       Sequel.connect(ENV['HEROKU_POSTGRESQL_ONYX_URL']) do |db|
-        db[table_name].truncate
-        db[table_name].import(columns, data)
+        db[table_name.to_sym].truncate
+        db[table_name.to_sym].import(columns, data)
       end
 
       nil
