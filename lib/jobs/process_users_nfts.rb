0f9612c759d679947aa48e30b28f62f384897846
dax <d.dax@email.com>
2022-11-19 13:59:44 +0100

    Update process nfts job

--- lib/jobs/process_users_nfts.rb
+++ lib/jobs/process_users_nfts.rb
@@ -3,7 +3,23 @@ module Jobs
     sidekiq_options queue: 'processing'
 
     def perform
-      Models::User.where(guest: false).each_slice(1000) do |users|
+      # always perform this on non-guest users where there is no nfts model yet
+      addresses_with_no_nft_model = FAT_BOY_DATABASE[
+        "select t1.address
+        from users t1
+        where t1.guest = false
+        left join user_nfts t2 on t1.address = t2.address
+        where t2.id is null"
+      ].all.flat_map(&:values)
+
+      addresses_not_recently_updated = Models::UserNfts.stale.map(&:address)
+
+      [
+        addresses_with_no_nft_model,
+        addresses_not_recently_updated
+      ].
+      flatten.
+      each_slice(1000) do |addresses|
         users.each { |u| Jobs::ProcessUserNfts.perform_async(address: u.address) }
       end
     end
