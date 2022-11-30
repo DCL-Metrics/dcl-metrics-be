c48348ccd7fc959c89bed42369fdb421bd0b83ed
dax <d.dax@email.com>
2022-11-30 15:45:26 +0100

    Update schema annotation

--- lib/models/serialized_daily_scene_stats.rb
+++ lib/models/serialized_daily_scene_stats.rb
@@ -1,11 +1,12 @@
 # primary_key :id
 #
 # Date    :date,            null: false
+# String  :timeframe
 # Jsonb   :data_json,       null: false
 #
 # Time    :created_at,      null: false
 #
-# add_index :serialized_daily_scene_stats, [:date], unique: true
+# add_index :serialized_daily_scene_stats, [:date, :timeframe], unique: true
 
 module Models
   class SerializedDailySceneStats < Sequel::Model
