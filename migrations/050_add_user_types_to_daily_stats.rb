6f62f339349be20f747761a1e54512cd22086c04
dax <d.dax@email.com>
2022-12-18 15:13:26 +0100

    Fix syntax error

--- migrations/050_add_user_types_to_daily_stats.rb
+++ migrations/050_add_user_types_to_daily_stats.rb
@@ -2,8 +2,8 @@ print "add columns to daily_stats table\n"
 
 Sequel.migration do
   change do
-    add_column :daily_stats :guest_users, Integer
-    add_column :daily_stats :named_users, Integer
-    add_column :daily_stats :new_users, Integer
+    add_column :daily_stats, :guest_users, Integer
+    add_column :daily_stats, :named_users, Integer
+    add_column :daily_stats, :new_users, Integer
   end
 end
