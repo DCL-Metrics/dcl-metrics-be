acfc402ab78c375989b3156ffc47639db26d81ea
dax <d.dax@email.com>
2023-01-13 14:12:01 +0100

    Remove sorting on global users serializer

--- lib/serializers/global/users.rb
+++ lib/serializers/global/users.rb
@@ -67,7 +67,6 @@ module Serializers
         data.
         all.
         last(10).
-        sort_by { |x| [x[:attribute], x[:name]] }. # this is mainly to fix a flapping test
         reverse
       end
 
