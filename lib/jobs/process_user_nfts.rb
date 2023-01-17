d07fddf76311a3d70819ea1aa3373e946d457a4e
dax <d.dax@email.com>
2023-01-17 16:26:29 +0100

    Remove note

--- lib/jobs/process_user_nfts.rb
+++ lib/jobs/process_user_nfts.rb
@@ -14,20 +14,5 @@ module Jobs
     def nft_data(address)
       Adapters::Dcl::NftData.call(address: address)
     end
-
-    # NOTE TODO this one goes the other way -
-    # pull the data and then make the updates based on the users in that data
-    # rather than pull updates for the given user
-    def dao_activity(address)
-      # Models::UserDaoActivity.create(dao_activity(address))
-      # wget -O test.csv --no-check-certificate
-      # "https://docs.google.com/spreadsheets/d/1FoV7TdMTVnqVOZoV4bvVdHWkeu4sMH5JEhp8L0Shjlo/gviz/tq?tqx=out:csv&sheet=Members"
-      #
-      # sheet members for total/delegated_vp, delegators and delegate
-      # sheet votes for first/last votes cast and votes cast
-      # sheet proposals for proposals authored
-      # sheet collections for collection creator
-      # sheet team for filling memberships_json / active dao committee member
-    end
   end
 end
