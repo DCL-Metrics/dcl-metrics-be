6c69eaad688ae38de1ff3663b81667beb69b0eaa
dax <d.dax@email.com>
2023-01-17 21:56:23 +0100

    Refactor

--- lib/jobs/process_users_in_snapshot.rb
+++ lib/jobs/process_users_in_snapshot.rb
@@ -5,12 +5,11 @@ module Jobs
     def perform(snapshot_id)
       snapshot = Models::PeersDump[snapshot_id]
       datestamp = snapshot.created_at.to_date.to_s
-      addresses = snapshot.data.flat_map { |x| x['address'] }.uniq
+      addresses = snapshot.data.flat_map { |x| x['address'] }.uniq.compact
       already_processed = Models::User.
         select(:address).
         where(address: addresses, last_seen: snapshot.created_at.to_date).
-        flat_map { |x| x.values.values }.
-        compact
+        flat_map { |x| x.values.values }
 
       processable = addresses - already_processed
       print "#{self.class.name}: processing #{processable.count} users\n"
