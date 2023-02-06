aaa1d991be1c2358d9d3af01649491932a13c4fa
dax <d.dax@email.com>
2023-02-06 18:34:12 +0100

    return basic data of top 50 scenes

--- lib/serializers/scenes.rb
+++ lib/serializers/scenes.rb
@@ -1,20 +1,21 @@
 module Serializers
   class Scenes
-    def self.serialize(scenes)
-      new(scenes).call
+    def self.serialize(scenes, basic_data_only: false)
+      new(scenes, basic_data_only).call
     end
 
-    def initialize(scenes)
+    def initialize(scenes, basic_data_only)
       @scenes = scenes
+      @basic_data_only = basic_data_only
     end
 
     def call
-      addresses = scenes.flat_map { |scene| scene.marathon_users.keys }.uniq
-      enriched_users = enrich_user_data(addresses)
+      enriched_users = build_enriched_users unless basic_data_only
 
       scenes.map do |scene|
-        {
+        base = {
           name: scene.name,
+          uuid: scene.scene_disambiguation_uuid,
           date: scene.date.to_s,
           map_url: scene.map_url,
           visitors: scene.unique_addresses,
@@ -27,16 +28,26 @@ module Serializers
           unique_logouts: scene.unique_logouts,
           complete_sessions: scene.complete_sessions,
           avg_complete_session_duration: scene.avg_complete_session_duration,
+        }
+
+        next base if basic_data_only?
+
+        base.merge({
           marathon_users: serialize_marathon_users(scene.marathon_users, enriched_users),
           time_spent_histogram: scene.time_spent_histogram,
           visitors_by_hour_histogram: scene.visitors_by_hour_histogram,
           parcels_heatmap: scene.parcels_heatmap
-        }
+        })
       end
     end
 
     private
-    attr_reader :scenes
+    attr_reader :scenes, :basic_data_only
+
+    def build_enriched_users
+      addresses = scenes.flat_map { |scene| scene.marathon_users.keys }.uniq
+      enrich_user_data(addresses)
+    end
 
     def serialize_marathon_users(marathon_users, enriched_users)
       marathon_users.map do |address, time_spent|
