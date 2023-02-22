40c5f24f06d1e82eed105de9a87bedd99ab6ab62
dax <d.dax@email.com>
2023-02-22 22:36:43 +0100

    Update allow list

--- server.rb
+++ server.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 
 class Server < Sinatra::Application
-  ALLOWED_ACCESS_IP = %w[99.80.183.117 99.81.135.32 95.90.235.137]
+  ALLOWED_ACCESS_IP = %w[99.80.183.117 99.81.135.32 95.90.237.179]
 
   # Ensure all requests come from a fixed IP
   before do
