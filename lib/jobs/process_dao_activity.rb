c8beb10cc9f99e50c4dffa79dc0412772889d847
dax <d.dax@email.com>
2023-03-24 16:52:47 +0100

    Reorder dao activity job and return nil

--- lib/jobs/process_dao_activity.rb
+++ lib/jobs/process_dao_activity.rb
@@ -3,8 +3,7 @@ module Jobs
     sidekiq_options queue: 'processing'
 
     def perform(sheet_name)
-      governance = Models::DaoGovernance.last
-      data = Adapters::Dcl::DaoTransparency::Client.fetch_data(sheet_name).to_json
+      data = Adapters::Dcl::DaoTransparency::Client.fetch_data(sheet_name)
 
       if sheet_name == 'Votes'
         # NOTE: votes has a LOT more data than the other sheets,
@@ -12,6 +11,8 @@ module Jobs
         data.each { |v| create_vote(v) }
       else
         sheet_name.downcase!
+        data = data.to_json
+        governance = Models::DaoGovernance.last
         existing_data = governance.public_send("#{sheet_name}_json")
 
         update_params = { "#{sheet_name}_updated_at" => Time.now.utc }
@@ -19,6 +20,8 @@ module Jobs
 
         governance.update(update_params)
       end
+
+      nil
     end
 
     private
