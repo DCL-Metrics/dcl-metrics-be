c8ffb359d879da7ad428847833d5851bd20cac9d
dax <d.dax@email.com>
2023-06-07 15:19:00 +0200

    Start tracking new data from worlds

--- lib/adapters/dcl/worlds.rb
+++ lib/adapters/dcl/worlds.rb
@@ -1,28 +1,49 @@
 module Adapters
   module Dcl
     class Worlds
-      CONTENT_SERVER_URL = "https://worlds-content-server.decentraland.org/status"
+      BASE_URL = 'https://worlds-content-server.decentraland.org/'
+      STATUS_ENDPOINT = 'status'
+      INDEX_ENDPOINT = 'index'
+      DETAILS_ENDPOINT = 'live-data'
 
       def self.call
         new.call
       end
 
       def call
-        content_server_response = Adapters::Base.get(CONTENT_SERVER_URL)
+        content_server_response = Adapters::Base.get(BASE_URL + STATUS_ENDPOINT)
         return notify_failure(content_server_response) if content_server_response.failure?
 
-        p content_server_response
+        index_response = Adapters::Base.get(BASE_URL + INDEX_ENDPOINT)
+        return notify_failure(index_response) if index_response.failure?
 
-        data = content_server_response.success
+        details_response = Adapters::Base.get(BASE_URL + DETAILS_ENDPOINT)
+        return notify_failure(details_response) if details_response.failure?
+
+        status = content_server_response.success
+        index = index_response.success['data']
+        details = details_response.success['data']['perWorld']
 
         {
-          world_count: data['content']['worldsCount'],
-          total_user_count: data['comms']['users'].to_i,
-          total_rooms: data['comms']['rooms'].to_i
+          world_count: status['content']['worldsCount'],
+          total_user_count: status['comms']['users'].to_i,
+          total_rooms: status['comms']['rooms'].to_i,
+          data: consolidate_data(index, details)
         }
       end
 
       private
+
+      def consolidate_data(index, details)
+        index.map do |data|
+          name = data['name']
+          world_details = details.detect { |x| x['worldName'] == name }
+
+          data.merge!({
+            'user_count' => world_details.nil? ? 0 : world_details['users'].to_i
+          })
+        end
+      end
 
       def notify_failure(response)
         Services::TelegramOperator.notify(
