acf0739616041014b55fc1b44f8dcb819ad92bf9
dax <d.dax@email.com>
2024-04-15 13:20:38 +0200

    Use raw sql query

--- lib/serializers/global/daily_stats.rb
+++ lib/serializers/global/daily_stats.rb
@@ -10,12 +10,15 @@ module Serializers
       end
 
       def call
-        base = Models::DailyStats.order(:date)
-        base = after_date.nil? ? base.all : base.where { date > after_date }
+        query = "select * from daily_stats"
+        query += " where date > '#{after_date}'" unless after_date.nil?
+        query += " order by date"
 
-        base.inject({}) do |result, row|
-          data = row.serialize.merge(degraded: degraded?(row.date))
-          result[row.date.to_s] = data
+        data = DATABASE_CONNECTION[query].all
+
+        data.inject({}) do |result, row|
+          data = serialize(row)
+          result[row[:date].to_s] = data
           result
         end
       end
@@ -23,8 +26,18 @@ module Serializers
       private
       attr_reader :after_date
 
-      def degraded?(date)
-        Services::DailyDataAssessor.call(date)
+      def serialize(row)
+        {
+          active_parcels: row[:total_active_parcels],
+          active_scenes: row[:total_active_scenes],
+          degraded: Services::DailyDataAssessor.call(row[:date]),
+          users: {
+            guest_users: row[:guest_users],
+            named_users: row[:named_users],
+            new_users: row[:new_users],
+            unique_users: row[:unique_users]
+          }
+        }
       end
     end
   end
