cba83b095cb2db18477624dc46d3e7a722cfdae4
dax <d.dax@email.com>
2024-10-24 18:24:19 +0200

    Roll my own retry

--- lib/jobs/save_scene_utilization.rb
+++ lib/jobs/save_scene_utilization.rb
@@ -4,18 +4,24 @@ module Jobs
 
     WebError = Class.new(::StandardError)
 
-    def perform(x, y, last_update_at, owner)
-      url = "https://places.decentraland.org/api/places?positions=#{x},#{y}"
-      place_data = Adapters::Base.get(url)
-      raise WebError unless place_data.success?
+    def perform(x, y, last_update_at, owner, count = 1)
+      return if count > 5
 
-      Models::Parcel.update_or_create(x: x, y: y) do |p|
-        p.active_deploy = !place_data.success['data'].empty?
-        p.owner = owner
-        p.last_update_at = Time.at(last_update_at / 1000)
+      begin
+        url = "https://places.decentraland.org/api/places?positions=#{x},#{y}"
+        place_data = Adapters::Base.get(url)
+        raise WebError unless place_data.success?
+
+        Models::Parcel.update_or_create(x: x, y: y) do |p|
+          p.active_deploy = !place_data.success['data'].empty?
+          p.owner = owner
+          p.last_update_at = Time.at(last_update_at / 1000)
+        end
+
+        nil
+      rescue WebError => e
+        Jobs::SaveSceneUtilization.perform_async(x, y, last_update_at, owner, count + 1)
       end
-
-      nil
     end
   end
 end
