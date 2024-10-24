afd5db821474ebf6d3a55492e1b8f419d99e3c02
dax <d.dax@email.com>
2024-10-24 16:01:34 +0200

    Guard against unsuccessful place data result

--- lib/jobs/save_scene_utilization.rb
+++ lib/jobs/save_scene_utilization.rb
@@ -6,10 +6,12 @@ module Jobs
       url = "https://places.decentraland.org/api/places?positions=#{x},#{y}"
       place_data = Adapters::Base.get(url)
 
-      Models::Parcel.update_or_create(x: x, y: y) do |p|
-        p.active_deploy = !place_data.success['data'].empty?
-        p.owner = owner
-        p.last_update_at = Time.at(last_update_at / 1000)
+      if place_data.success?
+        Models::Parcel.update_or_create(x: x, y: y) do |p|
+          p.active_deploy = !place_data.success['data'].empty?
+          p.owner = owner
+          p.last_update_at = Time.at(last_update_at / 1000)
+        end
       end
 
       nil
