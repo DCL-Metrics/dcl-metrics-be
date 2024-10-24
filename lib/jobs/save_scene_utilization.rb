5dbb6ffdb76d8d40b877b904d1a3198b0ac6cec2
dax <d.dax@email.com>
2024-10-24 16:42:07 +0200

    Always save parcel data

--- lib/jobs/save_scene_utilization.rb
+++ lib/jobs/save_scene_utilization.rb
@@ -5,13 +5,12 @@ module Jobs
     def perform(x, y, last_update_at, owner)
       url = "https://places.decentraland.org/api/places?positions=#{x},#{y}"
       place_data = Adapters::Base.get(url)
+      active_deploy = place_data.success? ? !place_data.success['data'].empty? : false
 
-      if place_data.success?
-        Models::Parcel.update_or_create(x: x, y: y) do |p|
-          p.active_deploy = !place_data.success['data'].empty?
-          p.owner = owner
-          p.last_update_at = Time.at(last_update_at / 1000)
-        end
+      Models::Parcel.update_or_create(x: x, y: y) do |p|
+        p.active_deploy = active_deploy
+        p.owner = owner
+        p.last_update_at = Time.at(last_update_at / 1000)
       end
 
       nil
