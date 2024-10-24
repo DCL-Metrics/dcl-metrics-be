695aa2d64600e76c92b949f6f2fa5777948f4153
dax <d.dax@email.com>
2024-10-24 20:53:30 +0200

    stop job

--- lib/jobs/fetch_scene_utilization.rb
+++ lib/jobs/fetch_scene_utilization.rb
@@ -3,32 +3,33 @@ module Jobs
     sidekiq_options queue: 'processing'
 
     def perform
-      result = ""
-      conn = Faraday.new('https://api.decentraland.org/v2/tiles?include=id,updatedAt,owner')
+      return
+      # result = ""
+      # conn = Faraday.new('https://api.decentraland.org/v2/tiles?include=id,updatedAt,owner')
 
-      # pull this as chunks to make the download faster. tiles.json is ~25MB
-      conn.get do |req|
-        req.options.on_data = Proc.new do |chunk, size|
-          result << chunk.to_s.encode('UTF-8', invalidndef: :replace, replace: '?')
-        end
-      end
+      # # pull this as chunks to make the download faster. tiles.json is ~25MB
+      # conn.get do |req|
+      #   req.options.on_data = Proc.new do |chunk, size|
+      #     result << chunk.to_s.encode('UTF-8', invalidndef: :replace, replace: '?')
+      #   end
+      # end
 
-      data = JSON.parse(result)['data']
-      return nil if data.empty?
+      # data = JSON.parse(result)['data']
+      # return nil if data.empty?
 
-      data.values.each do |row|
-        x, y = row['id'].split(',')
-        last_update_at = row['updatedAt']
-        owner = row['owner'].downcase
+      # data.values.each do |row|
+      #   x, y = row['id'].split(',')
+      #   last_update_at = row['updatedAt']
+      #   owner = row['owner'].downcase
 
-        Jobs::SaveSceneUtilization.perform_async(x, y, last_update_at, owner)
-        sleep 0.2 # testing this out
-      end
+      #   Jobs::SaveSceneUtilization.perform_async(x, y, last_update_at, owner)
+      #   sleep 0.2 # testing this out
+      # end
 
-      # notify telegram that all jobs were fired (temporary)
-      Services::TelegramOperator.notify(level: :info, message: 'FetchSceneUtilization complete')
+      # # notify telegram that all jobs were fired (temporary)
+      # Services::TelegramOperator.notify(level: :info, message: 'FetchSceneUtilization complete')
 
-      nil
+      # nil
     end
   end
 end
