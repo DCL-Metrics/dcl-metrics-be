941ddf0c9370e0dbeaa95f01bb04b34d8bd120a6
dax <d.dax@email.com>
2024-10-25 21:50:38 +0200

    Attempt jobs again

--- lib/jobs/save_scene_utilization.rb
+++ lib/jobs/save_scene_utilization.rb
@@ -5,24 +5,23 @@ module Jobs
     WebError = Class.new(::StandardError)
 
     def perform(x, y, last_update_at, owner, count = 1)
-      # return if count > 5
-      return 'stopping'
+      return if count > 5
 
-      begin
-        url = "https://places.decentraland.org/api/places?positions=#{x},#{y}"
-        place_data = Adapters::Base.get(url)
-        raise WebError unless place_data.success?
+      url = "https://places.decentraland.org/api/places?positions=#{x},#{y}"
+      place_data = Adapters::Base.get(url)
+      raise WebError unless place_data.success?
 
+      if place_data.success?
         Models::Parcel.update_or_create(x: x, y: y) do |p|
           p.active_deploy = !place_data.success['data'].empty?
           p.owner = owner
           p.last_update_at = Time.at(last_update_at / 1000)
         end
-
-        nil
-      rescue WebError => e
+      else
         Jobs::SaveSceneUtilization.perform_async(x, y, last_update_at, owner, count + 1)
       end
+
+      nil
     end
   end
 end
