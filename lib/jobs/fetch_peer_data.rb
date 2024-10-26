638a3756c3f8c0a59da30aa0a1400131f9f53269
dax <d.dax@email.com>
2024-10-26 08:00:19 +0200

    Run save scene utilization on fetch peer data

--- lib/jobs/fetch_peer_data.rb
+++ lib/jobs/fetch_peer_data.rb
@@ -8,13 +8,17 @@ module Jobs
       coordinates = data.map { |c| c['parcel']&.join(',') }.compact.uniq
       scenes = Services::FetchSceneData.call(coordinates: coordinates)
 
-      # enrich peer data with scene cid
       data.each do |d|
         next unless d['parcel']
+
+        # track utilization for each parcel in data packet
+        Jobs::SaveSceneUtilization.peform_async(*d['parcel'])
         parcels = d['parcel'].join(',')
 
         scene = scenes.detect { |s| s.coordinates.split(';').include?(parcels) }
         next if scene.nil? # empty parcel
+
+        # enrich peer data with scene cid
         d['scene_cid'] = scene.cid
       end
 
