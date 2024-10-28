bbc0716ddc697dd027f340065fc7217ff27895ad
dax <d.dax@email.com>
2024-10-28 12:10:35 +0100

    Update how parcel data is saved

--- lib/jobs/save_scene_utilization.rb
+++ lib/jobs/save_scene_utilization.rb
@@ -5,7 +5,7 @@ module Jobs
     def perform(x, y, last_update_at = nil, owner = nil, count = 1)
       return if count > 3
 
-      parcel = Models::Parcel.find(x: x, y: y)
+      parcel = Models::Parcel.find_or_create(x: x, y: y)
       # don't check more than once a day
       return if parcel.utilization_last_checked_at > Time.now.utc - 60 * 60 * 24
 
@@ -13,18 +13,22 @@ module Jobs
       place_data = Adapters::Base.get(url)
 
       if place_data.success?
-        Models::Parcel.update_or_create(x: x, y: y) do |p|
-          p.active_deploy = !place_data.success['data'].empty?
-          p.owner = owner unless owner.nil?
-          p.last_update_at = Time.at(last_update_at / 1000) unless last_update_at.nil?
-          p.utilization_last_checked_at = Time.now.utc
-        end
+        update_params = {
+          active_deploy: !place_data.success['data'].empty?,
+          utilization_last_checked_at: Time.now.utc
+        }
+
+        update_params[:owner] = owner unless owner.nil?
+        update_params[:last_update_at] = Time.at(last_update_at / 1000) unless last_update_at.nil?
+
+        parcel.update(update_params)
       else
         p '##################################################'
         p '##################################################'
         p [x, y] => place_data.failure
         p '##################################################'
         p '##################################################'
+
         sleep 2
         Jobs::SaveSceneUtilization.perform_async(x, y, last_update_at, owner, count + 1)
       end
